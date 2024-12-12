8781e29a56e277f7d3874ea764587e9dfc18ab11
Sandro Camastral <sandro@unique.ch>
2024-12-12 13:56:19 +0100

    chore(cleanup): remove single-rg tf modules

--- terraform-modules-single-rg/jumpbox/cloud-init-scripts-2024-04/00-start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-# All scripts in this directory are concatenated and run as cloud-init script. This is thead head of it.
--- terraform-modules-single-rg/postgres/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "server_id" {
-  value = azurerm_postgresql_flexible_server.this.id
-}
-output "database_keyvault_id" {
-  value = azurerm_key_vault.this.id
-}
