e88b2f874dbf69513b532d274c72a8a66fe5fa49
unique-sandro <124629881+unique-sandro@users.noreply.github.com>
2025-12-09 06:28:26 +0000

    ci: update release bundle

--- terraform-modules/example_chat.tf
+++ terraform-modules/example_chat.tf
@@ -360,6 +360,6 @@ module "speech_service" {
   }
 }
 module "sharepoint-connector-secrets" {
-  source       = "github.com/unique-ag/connectors.git//services/sharepoint-connector/deploy/terraform/azure/sharepoint-connector-secrets?depth=1&ref=sharepoint-connector%40v2.0.0-beta.2"
+  source       = "github.com/unique-ag/connectors.git//services/sharepoint-connector/deploy/terraform/azure/sharepoint-connector-secrets?depth=1&ref=sharepoint-connector%40v2.0.0-beta.6"
   key_vault_id = module.automation.keyvault_id
 }
